f92a1996e3666823af5a39e4cc5f474fe35b43f8
Philip Holzmann <117722701+aurivus-ph@users.noreply.github.com>
2023-01-20 14:56:52 +0100

    ci!

--- ci/build-wheel-deps.sh
+++ ci/build-wheel-deps.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 set -e -x
 
-PACKAGES=(
-    libpng-1.6.37
-    freetype-2.12.1
-    harfbuzz-2.6.8
-)
+curl -L "https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz" -o libpng-1.6.37.tar.gz
+tar zxf libpng-1.6.37.tar.gz
+pushd libpng-1.6.37
+./configure
+make -j $(nproc)
+make install
+popd
 
-URLS=(
-    https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.gz
-    https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.gz
-    https://github.com/harfbuzz/harfbuzz/archive/refs/tags/2.6.8.tar.gz
-)
+curl -L "https://download.savannah.gnu.org/releases/freetype/freetype-2.12.1.tar.gz" -o freetype-2.12.1.tar.gz
+tar zxf freetype-2.12.1.tar.gz
+pushd freetype-2.12.1
+./configure
+make -j $(nproc)
+make install
+popd
 
+curl -L "https://github.com/harfbuzz/harfbuzz/archive/refs/tags/2.6.8.tar.gz" -o harfbuzz-2.6.8.tar.gz
+tar zxf harfbuzz-2.6.8.tar.gz
+pushd harfbuzz-2.6.8
 
-for idx in {0..2}; do
-    PACK=${PACKAGES[$idx]}
-    URL=${URLS[$idx]}
-    curl -L "${URL}" -o ${PACK}.tar.gz
-    tar zxf ${PACK}.tar.gz
-    pushd ${PACK}
-    ./configure
-    make
-    make install
-    popd
-done
+# missing macro in the locally installed automake
+cp /usr/share/aclocal/pkg.m4 /usr/local/share/aclocal
+
+./autogen.sh
+./configure
+make -j $(nproc)
+make install
+popd
